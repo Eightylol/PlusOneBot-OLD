5746c43ac3e92dd35108231c8ff9f7c8de767930
Dan-Levi Tømta <danbannan@gmail.com>
2017-03-07 07:05:16 +0100

    ...

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,23 @@
 $(function() {
-  let btn_servers = $('#btn_servers'),
-      btn_sign_out = $('#btn_sign_out')
+  $.post('/messages', function(data) {
+		$('#debug').html(JSON.stringify(data,null,2))
+		data.forEach(message => {
+			$('.log-messages').append(() => {
+				let messageElem = $('<div class="message-entry">')
+				let messageAvatar = $('<img src="' + (message.avatar != null ? message.avatar : "/public/img/no-avatar.png")		 + '">').appendTo(messageElem)
 
-  btn_servers.on('click',function(e) {
-    alert("Clicked Servers button")
-  })
-  btn_sign_out.on('click', function(e) {
-    alert("Clicked Sign out button")
-  })
+				let messageInner = $('<div class="inner">')
+
+				let messageHeader = $('<h3 class="title">')
+				messageHeader.html(message.username + ' <small><span class="muted"><i>in</i></span> ' + message.channel + " @ " + '<span class="time">'+ $.timeago(message.createdAt) +'</span>' + "</small>")
+				let messageContent = $('<div class="content">')
+				messageContent.text(message.content)
+
+				messageHeader.add(messageContent).appendTo(messageInner)
+				messageInner.appendTo(messageElem)
+				return messageElem
+				console.log(messageElem)
+			})
+		})
+  });
 })
